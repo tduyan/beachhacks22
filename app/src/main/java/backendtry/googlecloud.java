7cd1f5ed645a802661592eb4ddbd039457637de5
TDA <72992901+sooada@users.noreply.github.com>
2022-04-10 10:39:27 -0700

    firebase to android studio

---
+++ app/src/main/java/backendtry/googlecloud.java
@@ -0,0 +1,12 @@
+package backendtry;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+
+public class googlecloud {
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    // Create a storage reference from our app
+    StorageReference storageRef = storage.getReference();
+    
+    storageref.getBucket().objects.list(bucketName)
+}
